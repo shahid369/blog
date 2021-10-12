9b9834f792a59c3fa06b2c8aefe4a76bf58f1e12
shahid369 <madbroshahid@gmail.com>
2021-10-12 22:35:25 +0530

    first commit

---
+++ posts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(PostView)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(User)
+
+
+
